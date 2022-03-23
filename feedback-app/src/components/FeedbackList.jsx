ecde55fa9d506cf0fd5845d23819b0aa6aa0ee18
aMarshmallows <amasha667@gmail.com>
2022-03-23 15:44:17 -0400

    add spinner

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import {useContext} from 'react'
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 function FeedbackList() {
 
-    const {feedback} = useContext(FeedbackContext)
-    // if no feedback was passed in, do this
-    if (!feedback || feedback.length === 0) {
-        return <p>No Feedback</p>
-    }
+  const {feedback, isLoading} = useContext(FeedbackContext)
+  // if no feedback was passed in, do this
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+      return <p>No Feedback</p>
+  }
 
-  return (
+  return isLoading ? <Spinner></Spinner> : (
     <div className='feedback-list'>
-         {/* for every item in feedback items ('item' is an iterator) */}
+          {/* for every item in feedback items ('item' is an iterator) */}
         {feedback.map((item) => (
             // passing in key and item params to the feedback item component
             //  handle delete is now a prop passed into feedbackItem
@@ -24,6 +25,9 @@ function FeedbackList() {
         ))}
     </div>
   )
+
+
+ 
 }
 
 
